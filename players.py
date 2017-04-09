d34989358098a3ebcb3cd4d41558defe9027faf5
Daniel Suissa <danielsuissa@Daniels-MacBook-Pro.local>
2017-04-08 20:33:41 -0400

    AI tested and fixed

--- players.py
+++ players.py
@@ -1,5 +1,6 @@
 
 class AIPlayer:
+	
 	def __init__(self, token,difficulty = 'medium'):
 		'''
 		difficulty determines cutoff
@@ -9,55 +10,75 @@ class AIPlayer:
 		self.token = token
 		self.name = 'AI PLayer'
 		self.num_moves = 0
-	def adjust_cutoff(self):
-		if self.num_moves > 1:
-			self.cutoff += 1
-		if self.num_moves > 3:
-			self.cutoff += 2
-		if self.num_moves > 5:
-			self.cutoff += 2
+		if self.token == 'X':
+			self.opponent = 'O'
+		else:
+			self.opponent = 'X'
 	def move(self,state):
 		'''
 		'''
-		v,move = self.max(state,state.min_util,state.max_util,0)
+		did_cutoff = False
+		depth_reached = 0
+		nodes_count = 0
+		pruning_max = 0
+		pruning_min = 0
+		v,move,did_cutoff,depth_reached,nodes_count,pruning_max,pruning_min = self.max(state,state.min_util,state.max_util,0,did_cutoff,depth_reached,nodes_count,pruning_max,pruning_min)
 		state.make_move(*move)
-		self.num_moves += 1
-	def max(self,state,alpha,beta,depth):
+		print('cutoff: ', did_cutoff, ' depth_reached: ', depth_reached, '#nodes: ', nodes_count, ' max pruning: ', pruning_max, ' min_pruning: ', pruning_min)
+		self.num_moves += 2
+		self.cutoff+= 1
+	def max(self,state,alpha,beta,depth,did_cutoff,depth_reached,nodes_count,pruning_max,pruning_min):
 		'''
 			max
 		'''
+		depth_reached += 1
 		c_t = state.check_terminal()
 		if c_t[0] == 'terminal' or depth >= self.cutoff:
-			return (c_t[1],state.last_action) #the utility value / eval value
+			if depth >= self.cutoff:
+				did_cutoff = True
+			return (c_t[1],state.last_action,did_cutoff,depth_reached,nodes_count,pruning_max,pruning_min) #the utility value / eval value
 		v = state.min_util
 		best_action = None
+		depth_reached_temp = depth_reached #remember depth reached to not overincrement
 		for successor in state.possible_successors(self.token):
-			new_v = max(v,self.min(successor,alpha,beta,depth+1)[0])
+			depth_reached = depth_reached_temp
+			nodes_count += 1
+			new_v,move,did_cutoff,depth_reached,nodes_count,pruning_max,pruning_min = self.min(successor,alpha,beta,depth+1,did_cutoff,depth_reached,nodes_count,pruning_max,pruning_min)
+			new_v = max(v,new_v)
 			if new_v != v or best_action == None:
 				best_action = successor.last_action
 				v = new_v
 			if v >= beta:
-				return (v,best_action)
+				pruning_max += 1
+				return (v,best_action,did_cutoff,depth_reached,nodes_count,pruning_max,pruning_min)
 			alpha = max(alpha,v)
-		return (v,best_action)
-	def min(self,state,alpha,beta,depth):
+		return (v,best_action,did_cutoff,depth_reached,nodes_count,pruning_max,pruning_min)
+	def min(self,state,alpha,beta,depth,did_cutoff,depth_reached,nodes_count,pruning_max,pruning_min):
 		'''
 			min
 		'''
+		depth_reached += 1
 		c_t = state.check_terminal()
 		if c_t[0] == 'terminal' or depth >= self.cutoff:
-			return (c_t[1],state.last_action) #the utility value / eval value
+			#print('reached end. eval is: ', c_t[1])
+			if depth >= self.cutoff:
+				did_cutoff = True
+			return (c_t[1],state.last_action,did_cutoff,depth_reached,nodes_count,pruning_max,pruning_min) #the utility value / eval value
 		v = state.max_util
 		best_action = None
-		for successor in state.possible_successors(self.token):
-			new_v = min(v,self.max(successor,alpha,beta,depth+1)[0])
-			if new_v != v or best_action == None:
-				best_action = successor.last_action
-				v = new_v
+		depth_reached_temp = depth_reached
+		for successor in state.possible_successors(self.opponent):
+			depth_reached = depth_reached_temp
+			nodes_count += 1
+			new_v,move,did_cutoff,depth_reached,nodes_count,pruning_max,pruning_min = self.max(successor,alpha,beta,depth+1,did_cutoff,depth_reached,nodes_count,pruning_max,pruning_min)
+			new_v = min(v,new_v)
+			best_action = successor.last_action
+			v = new_v
 			if v <= alpha:
-				return (v,best_action)
+				pruning_min += 1
+				return (v,best_action,did_cutoff,depth_reached,nodes_count,pruning_max,pruning_min)
 			beta = max(beta,v)
-		return (v,best_action)
+		return (v,best_action,did_cutoff,depth_reached,nodes_count,pruning_max,pruning_min)
 class HumanPlayer:
 	def __init__(self,name="Player1",token='X'):
 		self.name = name
